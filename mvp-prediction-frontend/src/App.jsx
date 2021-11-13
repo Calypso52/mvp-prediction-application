ecd7a2ec18f997d49983cee7b7f20dcd2f5f0a01
zyt <243080140@qq.com>
2021-11-13 01:24:22 -0500

    playerName

--- mvp-prediction-frontend/src/App.jsx
+++ mvp-prediction-frontend/src/App.jsx
@@ -1,12 +1,48 @@
 import React, { Component } from 'react'
-import { Container, Button, Card, Nav, Form } from 'react-bootstrap'
+import Search from './component/Search'
+import Card from './component/Card'
+import { v4 as uuid } from 'uuid'
+import './App.css'
+import StatisticBox from './component/StatisticBox'
+import PredictionBox from './component/PredictionBox'
 
 export default class App extends Component {
+  state = {
+    // 远程搜索下拉框数据
+    players: [
+      { id: uuid(), name: 'James' },
+      { id: uuid(), name: 'Durant' },
+      { id: uuid(), name: 'Harden' },
+      { id: uuid(), name: 'Paul' },
+      { id: uuid(), name: 'Westbrook' },
+      { id: uuid(), name: 'Davis' },
+      { id: uuid(), name: 'Anthony' }
+    ],
+    // 球员各项数据
+    playerStatistic: [],
+    // 搜索结果控件 -- 是否第一次搜索
+    isSearchFirst: true,
+    // 搜索结果控件 -- 搜索结果是否处于loading状态
+    isSearchLoading: false
+  }
+
+  updateStatistic = (playerStatistic) => {
+    this.setState(playerStatistic)
+  }
+
   render() {
+    const { players } = this.state;
     return (
-      <Container>
-        
-      </Container>
+      <div className="outermostWrapper">
+          <Search
+            updateStatistic={ this.updateStatistic }
+          />
+          <Card
+            players={ players }
+          />
+          <StatisticBox/>
+          <PredictionBox/>
+      </div>
     )
   }
 }
--- mvp-prediction-frontend/src/index.js
+++ mvp-prediction-frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <BrowserRouter>
