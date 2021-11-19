43086c1ecc8b82ad4873f9502db4505adf3e2cde
zyt <243080140@qq.com>
2021-11-19 18:58:16 -0500

    add player card

--- test proxy server/server1.js
+++ test proxy server/server1.js
@@ -12,16 +12,16 @@ app.use((request,response,next)=>{
 
 app.get('/playerStatistic',(request,response)=>{
 	const playerStatistics = [
-		{id:uuid(), name:'LeBron James', pts: 24.8, reb: 5.5, ast: 7.0, stl: 2.3, blk: 0.8, tov: 4.2},
-		{id:uuid(), name:'Kevin Durant', pts: 29.5, reb: 8.5, ast: 5.0, stl: 0.6, blk: 0.7, tov: 3.5},
-		{id:uuid(), name:'Carmelo Anthony', pts: 17.2, reb: 3.8, ast: 1.0, stl: 0.8, blk: 0.9, tov: 0.8},
-		{id:uuid(), name:'Russel Westbrook', pts: 19.3, reb: 9.1, ast: 8.9, stl: 1.4, blk: 0.3, tov: 5.3},
-		{id:uuid(), name:'James Harden', pts: 19.5, reb: 7.6, ast: 9.3, stl: 1.2, blk: 0.8, tov: 4.9},
-		{id:uuid(), name:'Paul George', pts: 19.5, reb: 7.6, ast: 9.3, stl: 1.2, blk: 0.8, tov: 4.9},
-		{id:uuid(), name:'Giannis Antetokounmpo', pts: 19.5, reb: 7.6, ast: 9.3, stl: 1.2, blk: 0.8, tov: 4.9},
-		{id:uuid(), name:'Stephen Curry', pts: 19.5, reb: 7.6, ast: 9.3, stl: 1.2, blk: 0.8, tov: 4.9},
-		{id:uuid(), name:'Damian Lillard', pts: 19.5, reb: 7.6, ast: 9.3, stl: 1.2, blk: 0.8, tov: 4.9},
-		{id:uuid(), name:'Bradley Beal', pts: 19.5, reb: 7.6, ast: 9.3, stl: 1.2, blk: 0.8, tov: 4.9},
+		{id:uuid(), name:'LeBron James', pts: 24.8, reb: 5.5, ast: 7.0, stl: 2.3, blk: 0.8, tov: 4.2, src: 'https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png', teamSrc: 'https://cdn.nba.com/logos/nba/1610612747/primary/D/logo.svg', team: 'LosAngles Lakers'},
+		{id:uuid(), name:'Kevin Durant', pts: 29.5, reb: 8.5, ast: 5.0, stl: 0.6, blk: 0.7, tov: 3.5, src: 'https://cdn.nba.com/headshots/nba/latest/1040x760/201142.png', teamSrc: 'https://cdn.nba.com/logos/nba/1610612751/primary/D/logo.svg', team: 'Brooklyn Nets'},
+		{id:uuid(), name:'Carmelo Anthony', pts: 17.2, reb: 3.8, ast: 1.0, stl: 0.8, blk: 0.9, tov: 0.8, src: 'https://cdn.nba.com/headshots/nba/latest/260x190/2546.png', teamSrc: 'https://cdn.nba.com/logos/nba/1610612747/primary/D/logo.svg', team: 'LosAngles Lakers'},
+		{id:uuid(), name:'Russel Westbrook', pts: 19.3, reb: 9.1, ast: 8.9, stl: 1.4, blk: 0.3, tov: 5.3, src: 'https://cdn.nba.com/headshots/nba/latest/1040x760/201566.png', teamSrc: 'https://cdn.nba.com/logos/nba/1610612747/primary/D/logo.svg', team: 'LosAngles Lakers'},
+		{id:uuid(), name:'James Harden', pts: 19.5, reb: 7.6, ast: 9.3, stl: 1.2, blk: 0.8, tov: 4.9, src: 'https://cdn.nba.com/headshots/nba/latest/1040x760/201935.png', teamSrc: 'https://cdn.nba.com/logos/nba/1610612751/primary/D/logo.svg', team: 'Brooklyn Nets'},
+		{id:uuid(), name:'Paul George', pts: 19.5, reb: 7.6, ast: 9.3, stl: 1.2, blk: 0.8, tov: 4.9, src: 'https://cdn.nba.com/headshots/nba/latest/1040x760/202331.png', teamSrc: 'https://cdn.nba.com/logos/nba/1610612746/primary/D/logo.svg', team: 'LosAngles Clippers'},
+		{id:uuid(), name:'Giannis Antetokounmpo', pts: 19.5, reb: 7.6, ast: 9.3, stl: 1.2, blk: 0.8, tov: 4.9, src: 'https://cdn.nba.com/headshots/nba/latest/260x190/203507.png', teamSrc: 'https://cdn.nba.com/logos/nba/1610612749/primary/D/logo.svg', team: 'Milwaukee Bucks'},
+		{id:uuid(), name:'Stephen Curry', pts: 19.5, reb: 7.6, ast: 9.3, stl: 1.2, blk: 0.8, tov: 4.9, src: 'https://cdn.nba.com/headshots/nba/latest/260x190/201939.png', teamSrc: 'https://cdn.nba.com/logos/nba/1610612744/primary/D/logo.svg', team: 'Golden State Worriors'},
+		{id:uuid(), name:'Damian Lillard', pts: 19.5, reb: 7.6, ast: 9.3, stl: 1.2, blk: 0.8, tov: 4.9, src: 'https://cdn.nba.com/headshots/nba/latest/260x190/203081.png', teamSrc: 'https://cdn.nba.com/logos/nba/1610612757/primary/L/logo.svg', team: 'Portland Trail Blazers'},
+		{id:uuid(), name:'Bradley Beal', pts: 19.5, reb: 7.6, ast: 9.3, stl: 1.2, blk: 0.8, tov: 4.9, src: 'https://cdn.nba.com/headshots/nba/latest/260x190/203078.png', teamSrc: 'https://cdn.nba.com/logos/nba/1610612764/primary/D/logo.svg', team: 'Washington Wizards	'},
 	]
 	response.send(playerStatistics)
 })
