c9357bdfd452525a5c0069e5dca9176ceb570078
zyt <243080140@qq.com>
2021-11-24 14:17:31 -0500

    filt at backend

--- mvp-prediction-frontend/src/component/StatisticBox/PlayerData/index.jsx
+++ mvp-prediction-frontend/src/component/StatisticBox/PlayerData/index.jsx
@@ -6,19 +6,19 @@ export default class PlayerData extends Component {
         const { playerStatistic } = this.props;
 
         return (
-            <div class="playerCardWrapper">
-                <div class="upperWrapper">
-                    <div class="teamBackgroundImage" style={{ background: `url('${playerStatistic.teamSrc}') center center no-repeat`, opacity: 0.1, height: '250px' }}></div>
-                    <div class="teamlogo"><img src={ playerStatistic.teamSrc } alt="teamLogo"/></div>
-                    <div class="playerLogo"><img src={ playerStatistic.src } alt="playerLogo"/></div>
-                    <div class="teamInformation"><p>{ playerStatistic.team }</p></div>
-                    <div class="playerName">
+            <div className="playerCardWrapper">
+                <div className="upperWrapper">
+                    <div className="teamBackgroundImage" style={{ background: `url('${playerStatistic.teamSrc}') center center no-repeat`, opacity: 0.1, height: '250px' }}></div>
+                    <div className="teamlogo"><img src={ playerStatistic.teamSrc } alt="teamLogo"/></div>
+                    <div className="playerLogo"><img src={ playerStatistic.src } alt="playerLogo"/></div>
+                    <div className="teamInformation"><p>{ playerStatistic.team }</p></div>
+                    <div className="playerName">
                         <p>{ playerStatistic.name.split(' ')[0] }</p>
                         <p>{ playerStatistic.name.split(' ')[1] }</p>
                     </div>
                 </div>
-                <div class="statisticWrapper">
-                    <ul class="statisticRow">
+                <div className="statisticWrapper">
+                    <ul className="statisticRow">
                         <li>
                             <p>PTS</p>
                             <p>{ playerStatistic.pts }</p>
