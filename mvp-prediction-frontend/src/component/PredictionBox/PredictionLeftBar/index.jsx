05ed39e2cb62d9ca53f5eab7e7d25753a7e31b83
zyt <243080140@qq.com>
2021-12-05 20:34:41 -0500

    display effect modify

--- mvp-prediction-frontend/src/component/PredictionBox/PredictionLeftBar/index.jsx
+++ mvp-prediction-frontend/src/component/PredictionBox/PredictionLeftBar/index.jsx
@@ -2,27 +2,90 @@ import React, { Component } from 'react'
 import './index.css'
 
 export default class PredictionLeftBar extends Component {
+    state = {
+        // 当前选中的选项序号
+        currentIndex: 0
+    }
+
+    setCurrentIndex = (event) => {
+        // 获取当前点的是哪一个li索引
+        let itemIndex = parseInt(event.currentTarget.getAttribute('index'), 10);
+        // 获取当前选中的奖项名称
+        let itemPrize = event.currentTarget.firstElementChild.nextElementSibling.innerHTML;
+        // 获取当前选中的奖项icon类
+        let itemIcon = event.currentTarget.firstElementChild.getAttribute('class');
+        // 将选中的li添加active类使其高亮
+        this.setState({ currentIndex: itemIndex });
+        // 更新选中的奖项
+        this.props.setCurrentPrize(itemPrize, itemIcon);
+        // 清空展示图像中的数据条
+        this.props.clearFigure();
+        // 执行查询预测结果函数
+        setTimeout(() => { this.sendPrediction(itemIndex) }, 500);
+    }
+
+    // 查询预测结果函数
+    sendPrediction = (itemIndex) => {
+        let percent;
+        switch(itemIndex) {
+            case 0: // 发送MVP预测
+                percent = 0.9;
+                break;
+            case 1: // 发送DPOY预测
+                percent = 0.755
+                break;
+            case 2: // 发送MIP预测
+                percent = 0.15
+                break;
+            default:
+                break;
+        }
+        this.props.percentage(percent);
+    }
+
     render() {
+        const prizeItemArr = [
+                                ['fas fa-trophy', 'MVP'], 
+                                ['fas fa-shield-alt', 'DPOY'], 
+                                ['fas fa-level-up-alt', 'MIP']
+                            ];
+        let itemList = [];
+        for(let i = 0; i < prizeItemArr.length; i++) {
+            itemList.push(
+                <li 
+                    key = { i }
+                    className={ `prediction-left-bar-item ${this.state.currentIndex === i ? 'active' : ''}` }
+                    index = { i }
+                    onClick={ this.setCurrentIndex }
+                >
+                    <i className={ prizeItemArr[i][0] }></i>
+                    <div className="prediction-prize">{ prizeItemArr[i][1] }</div>
+                    <div className="left-string"></div>
+                    <div className="prediction-active-Background"></div>
+                </li>
+            )
+        }
         return (
             <div className="prediction-left-bar">
                 <div className="prediction-left-bar-award">Awards Prediction</div>
                 <div></div>
                 <ul className="prediction-left-bar-menu">
-                    <li className="prediction-left-bar-item">
-                        <i className="fas fa-search"></i>
+                    {/* <li className="prediction-left-bar-item" ref={ c => this.itemMVP = c } onClick={ this.predictItemClick }>
+                        <i class="fas fa-trophy"></i>
                         <div className="prediction-prize">MVP</div>
+                        <div className="left-string"></div>
                     </li>
-                    <div></div>
-                    <li className="prediction-left-bar-item">
-                        <i className="fas fa-search"></i>
+                    <li className="prediction-left-bar-item" onClick={ this.predictItemClick }>
+                        <i class="fas fa-shield-alt"></i>
                         <div className="prediction-prize">DPOY</div>
+                        <div className="left-string"></div>
                     </li>
-                    <div></div>
-                    <li className="prediction-left-bar-item">
-                        <i className="fas fa-search"></i>
+                    <li className="prediction-left-bar-item" onClick={ this.predictItemClick }>
+                        <i class="fas fa-level-up-alt"></i>
                         <div className="prediction-prize">MIP</div>
-                    </li>
-                    <div></div>
+                        <div className="left-string"></div>
+                    </li> */}
+                    { itemList }
                 </ul>
             </div>
         )
--- mvp-prediction-frontend/src/component/PredictionBox/index.jsx
+++ mvp-prediction-frontend/src/component/PredictionBox/index.jsx
@@ -4,11 +4,86 @@ import PredictionMain from './PredictionMain'
 import './index.css'
 
 export default class PredictionBox extends Component {
+    state = {
+        // 当前选中的奖项名称
+        currentPrize: 'MVP',
+        // 当前选中的奖项icon
+        currentPrizeIcon: 'fas fa-trophy',
+        // 属于PredictionMain组件中的状态
+        rightTransform: '',
+        rightTransition: '',
+        rightOpacity: '',
+        leftTransform: '',
+        leftTransition: '',
+        percentageOpacity: 1,
+        resultPercentage: '0%'
+    }
+
+    setCurrentPrize = (itemPrize, itemIcon) => {
+        this.setState({ currentPrize: itemPrize, currentPrizeIcon: itemIcon });
+    }
+
+    percentage = (percent) => {
+        this.setState({ resultPercentage: percent * 100 + '%' });
+        let delayTime;
+        if (percent <= 0.5) {  //红色区域不超过一半
+            delayTime = 1100;
+            this.animationFirstStep(percent);
+        } else {    //红色区域超过一半的情况，重点部分
+            delayTime = percent * 2000;
+            this.animationSecondStep(percent);
+        }
+        setTimeout(() => {
+            this.animationThirdStep();
+        }, delayTime);
+    }
+
+    animationFirstStep = (percent) => {
+        this.setState({ rightTransform: `rotate(${percent * 360}deg)`,
+                        rightTransition: `opacity 0s step-end 1s, transform 1s linear`
+                        });
+    }
+
+    animationSecondStep = (percent) => {
+        this.setState({ rightTransform: `rotate(180deg)`,
+                        rightTransition: `opacity 0s step-end 1s, transform 1s linear`,
+                        rightOpacity: 0,
+                        leftTransition: `transform ${(percent - 0.5) / 0.5}s linear 1s`,
+                        leftTransform: `rotate(${percent * 360 - 180}deg)`
+                        });
+    }
+
+    animationThirdStep = () => {
+        this.setState({ percentageOpacity: 1 });
+    }
+
+    // 将图像恢复初始值
+    clearFigure = () => {
+        this.setState({
+            rightTransform: '',
+            rightTransition: 'transform 0s linear',
+            rightOpacity: '',
+            leftTransform: '',
+            leftTransition: '',
+            percentageOpacity: 0
+        })
+    }
+
     render() {
         return (
             <div className="pred-outerwrapper">
-                <PredictionLeftBar/>
-                <PredictionMain/>
+                <PredictionLeftBar
+                    setCurrentPrize={ this.setCurrentPrize }
+                    clearFigure={ this.clearFigure }
+                    percentage={ this.percentage }
+                    {...this.state}
+                />
+                <PredictionMain
+                    animationFirstStep={ this.animationFirstStep }
+                    animationSecondStep={ this.animationSecondStep }
+                    animationThirdStep={ this.animationThirdStep }
+                    {...this.state}
+                />
             </div>
         )
     }
